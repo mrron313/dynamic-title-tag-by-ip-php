ea10129b829df717360f4522aaf26e2a117854e7
mrron19 <ez.boy763@gmail.com>
2019-08-23 11:07:09 +0600

    first commit

---
+++ function.php
@@ -0,0 +1,61 @@
+<?php
+// Change Title Tag Version 1.5
+function ch_title($title)
+{
+    $output = ob_get_contents();
+    if (ob_get_length() > 0) {ob_end_clean();}
+    $patterns = array("/<title>(.*?)<\/title>/");
+    $replacements = array("<title>$title</title>");
+    $output = preg_replace($patterns, $replacements, $output);
+    echo $output;
+}
+
+function get_client_ip()
+{
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP')) {
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    } else if (getenv('HTTP_X_FORWARDED_FOR')) {
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    } else if (getenv('HTTP_X_FORWARDED')) {
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    } else if (getenv('HTTP_FORWARDED_FOR')) {
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    } else if (getenv('HTTP_FORWARDED')) {
+        $ipaddress = getenv('HTTP_FORWARDED');
+    } else if (getenv('REMOTE_ADDR')) {
+        $ipaddress = getenv('REMOTE_ADDR');
+    } else {
+        $ipaddress = 'UNKNOWN';
+    }
+
+    return $ipaddress;
+}
+
+function getClientIpDetails()
+{
+    $PublicIP = get_client_ip();
+
+    $url = 'https://api.snoopi.io/'. $PublicIP;
+    $ch = curl_init();
+    curl_setopt ($ch, CURLOPT_URL, $url);
+    curl_setopt ($ch, CURLOPT_CONNECTTIMEOUT, 5);
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+    $contents = curl_exec($ch);
+    if (curl_errno($ch)) {
+      echo curl_error($ch);
+      echo "\n<br />";
+      $contents = '';
+    } else {
+      curl_close($ch);
+    }
+    
+    if (!is_string($contents) || !strlen($contents)) {
+        echo "Failed to get contents.";
+        $contents = '';
+    }
+
+
+    return $contents;
+}
+
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    ob_start();
+?>
+
+<?php include 'header.php';
+
+    ch_title("Dashboard");
+
+    getClientIpDetails();
+?>
+
+<?php include 'footer.php';?>
